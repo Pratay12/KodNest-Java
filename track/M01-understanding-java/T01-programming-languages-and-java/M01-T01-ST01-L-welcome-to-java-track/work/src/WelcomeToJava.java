c49a200ed9b854fc5fc7c7ec50a3ffffb3b3c391
pratay12 <koleypratay@gmail.com>
2026-09-06 13:11:59 +0530

    Update WelcomeToJava with println statements

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
-       System.out.print("my name is pratay");
-       System.out.print("i am a java track");
+       System.out.println("my name is pratay");
+       System.out.println("i am a java track");
     }
 }
