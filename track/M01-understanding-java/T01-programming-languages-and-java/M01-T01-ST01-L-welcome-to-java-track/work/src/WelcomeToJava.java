a6481717c55150f46f3cb3b6a32defcb9d8d847b
pratay12 <koleypratay@gmail.com>
2026-09-06 12:53:01 +0530

    this is my first edit

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,5 +2,6 @@ public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
        System.out.print("my name is pratay");
+       System.out.print("i am a java track");
     }
 }
